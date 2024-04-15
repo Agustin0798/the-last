740edf8783265f896ac88eab93ea42cf54f4ec21
jerojimenez17 <jerodepiran@gmail.com>
2024-04-15 01:20:03 -0300

    agregando parametro valor

--- operandos.h
+++ operandos.h
@@ -3,7 +3,7 @@
 #include "MV.h"
 
 int getOperando(MV mv, char op, char iptemp);
-void setOperando(MV mv, char op);
+void setOperando(MV mv, char op, int valor);
 int memoria(MV mv, int value);
 int registro(MV mv, int);
 int leerParametro(MV mv, char op);
